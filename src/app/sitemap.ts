8ded15453b84f4f0f098e34a8726afa142f0cc15
Farhan Ahmed <farhanahmedmirali@gmail.com>
2026-04-12 13:38:39 +0500

    updated Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,26 @@
 export default function sitemap() {
+  const lastModified = new Date();
+
   return [
-    { url: "https://farhanahmed.vercel.app/home", lastModified: new Date() },
-    { url: "https://farhanahmed.vercel.app/about", lastModified: new Date() },
-    { url: "https://farhanahmed.vercel.app/skills", lastModified: new Date() },
-    { url: "https://farhanahmed.vercel.app/contact", lastModified: new Date() },
-    { url: "https://farhanahmed.vercel.app/projects", lastModified: new Date() },
+    {
+      url: "https://farhanahmed.vercel.app/",
+      lastModified,
+    },
+    {
+      url: "https://farhanahmed.vercel.app/about",
+      lastModified,
+    },
+    {
+      url: "https://farhanahmed.vercel.app/skills",
+      lastModified,
+    },
+    {
+      url: "https://farhanahmed.vercel.app/contact",
+      lastModified,
+    },
+    {
+      url: "https://farhanahmed.vercel.app/projects",
+      lastModified,
+    },
   ];
 }
